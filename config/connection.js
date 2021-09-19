2f34ca21ad30a17071a8d8aab939ddeb88e4e3b4
Boba Fett <thomascupchurch@gmail.com>
2021-09-19 17:55:28 -0500

    another fix to jawsdb and favicon issue

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database, pass in your MySql information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
 
 let sequelize;
 
