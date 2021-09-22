7c24af6c84a57e62353b673d5be339de55ecedc2
Boba Fett <thomascupchurch@gmail.com>
2021-09-22 12:01:09 -0500

    fix multiple plurality issues with table names

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,25 +1,27 @@
 async function newFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
-    const contents = document.querySelector('textarea[name="post-url"]').value;
+  const title = document.querySelector('input[name="post-title"]').value;
+  const contents = document.querySelector('textarea[name="post-url"]').value;
 
-    const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            contents
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  const response = await fetch("/api/post", {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      contents,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
