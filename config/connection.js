6578ca7b1ce8c8258c107ce42c495274f825ac09
Boba Fett <thomascupchurch@gmail.com>
2021-09-22 11:11:29 -0500

    fixed comment association problems

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,6 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our database, pass in your MySql information for username and password
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-// });
 
 let sequelize;
 
@@ -21,13 +15,6 @@ if(process.env.JAWSDB_URL) {
     });
 }
 
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     });
 
 
 module.exports = sequelize;
--- models/index.js
+++ models/index.js
@@ -1,45 +1,44 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
 // create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
 });
 
 // User.belongsToMany(Post, {
-  
+
 //     foreignKey: 'user_id'
 // });
 
 // Post.belongsToMany(User, {
-    
+
 //     foreignKey: 'post_id'
 // });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+  //   foreignKey: "user_id",
+  onDelete: "SET NULL",
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: 'SET NULL'
+  foreignKey: "post_id",
+  onDelete: "SET NULL",
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+  foreignKey: "post_id",
 });
-
 
 module.exports = { User, Post, Comment };
